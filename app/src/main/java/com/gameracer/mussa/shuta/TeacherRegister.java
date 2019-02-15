2561f24a03b3b3cd163d3b37c2c89d0babaf962e
Mussa Mchomvu <msmchomvu@gmail.com>
2019-02-15 11:41:14 +0300

    ver2

--- app/src/main/java/com/gameracer/mussa/shuta/TeacherRegister.java
+++ app/src/main/java/com/gameracer/mussa/shuta/TeacherRegister.java
@@ -3,16 +3,29 @@ package com.gameracer.mussa.shuta;
 import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.gameracer.mussa.shuta.provider.ShutaProvider;
 import com.gameracer.mussa.shuta.provider.Teacher;
 
 public class TeacherRegister extends AppCompatActivity {
+    private String gender;
+    private RadioButton teacherMale,teacherFemale;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_teacher);
+        initView();
     }
+
+    private void initView(){
+    teacherFemale=(RadioButton)findViewById(R.id.teacherFemale);
+    teacherMale=(RadioButton)findViewById(R.id.teacherMale);
+    }
+
 
     public void addTeacher(Teacher teacher) {
 
@@ -28,4 +41,21 @@ public class TeacherRegister extends AppCompatActivity {
         values.put(ShutaProvider.TEACHER_COLUMN_LOCATION, teacher.getLocationID() );
         getContentResolver().insert(ShutaProvider.SUBJECT_URI, values);
     }
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.studentMale:
+                if (checked) {
+                    gender =  teacherMale.getText().toString();
+                }
+                break;
+            case R.id.studentFemale:
+                if (checked)
+                    gender =  teacherFemale.getText().toString();
+                break;
+        }
+}
 }
